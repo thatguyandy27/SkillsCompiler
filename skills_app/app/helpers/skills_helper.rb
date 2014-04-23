4576948e3984541f8ac00ea1e6d74cd9f7e6b415
Andy M <andy.dude@gmail.com>
2014-04-23 18:08:57 -0500

    Fixed defect with special characters in tag

--- skills_app/app/helpers/skills_helper.rb
+++ skills_app/app/helpers/skills_helper.rb
@@ -3,7 +3,7 @@ module SkillsHelper
   def careers_link(skill_name, link_name=nil, options={})
     options[:class] ||= "button"
     link_name ||= "Current #{skill_name} postings"
-    return link_to(link_name, "http://careers.stackoverflow.com/jobs/tag/" + skill_name,
+    return link_to(link_name, "http://careers.stackoverflow.com/jobs/tag/" + CGI::escape(skill_name),
       options)
   end
 
